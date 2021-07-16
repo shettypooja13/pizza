14aaf323f0a0df789ab53fdb3667ff5f5cad3090
Pooja Shetty <poojashetty1003@gmail.com>
2021-07-16 14:24:39 +0530

    add cart and auth

--- server.js
+++ server.js
@@ -5,14 +5,24 @@ const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 const path = require('path')
 
-app.get('/', (req,res) => {
-    res.render('home');
-})
-
 app.use(express.static("public"));
 app.use(expressLayout)
 app.set('views',path.join(__dirname, '/resources/views'))
 app.set('view engine','ejs')
+
+
+app.get('/', (req,res) => {
+    res.render('home');
+})
+app.get('/cart', (req,res) => {
+    res.render('customers/cart');
+})
+app.get('/login', (req,res) => {
+    res.render('auth/login');
+})
+app.get('/register',(req,res) => {
+    res.render('auth/register');
+})
 
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`)
